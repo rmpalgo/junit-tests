0eac4284d96a56059bb04a2ba3abdfa7a8461e4b
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-21 16:45:01 -0500

    Working with TDD examples using legacy code.

--- src/test/java/CohortTest.java
+++ src/test/java/CohortTest.java
@@ -13,33 +13,43 @@ public class CohortTest {
 
     @Before
     public void setup(){
+
         emptyCohort = new Cohort();
         cohortWithOne = new Cohort();
         cohortWithMany = new Cohort();
 
-        Student fer = new Student(1, "Fer M");
-        fer.addGrade(90);
+        Student ron = new Student(1, "Ron");
+        ron.addGrade(89);
 
-        Student trant = new Student(2, "Trant B");
-        trant.addGrade(100);
-        trant.addGrade(100);
-        trant.addGrade(100);
+        Student milo = new Student(2, "Milo");
+        milo.addGrade(81);
+        milo.addGrade(92);
+        milo.addGrade(99);
 
-        cohortWithOne.addStudent(fer);
-        cohortWithMany.addStudent(fer);
-        cohortWithMany.addStudent(trant);
+        Student mia = new Student(3, "Mia");
+        mia.addGrade(99);
+        mia.addGrade(97);
+        mia.addGrade(96);
+
+        cohortWithOne.addStudent(ron);
+        cohortWithMany.addStudent(ron);
+        cohortWithMany.addStudent(milo);
+        cohortWithMany.addStudent(mia);
     }
 
     @Test
     public void testAddStudentAndGetStudentsWork(){
         assertEquals(0, emptyCohort.getStudents().size());
         assertEquals(1, cohortWithOne.getStudents().size());
-//        assertEquals(1, cohortWithOne.getStudents().get(0).getId());
+        assertEquals(3, cohortWithMany.getStudents().size());
+        cohortWithMany.addStudent(new Student(3, "Jack"));
+        assertEquals(4, cohortWithMany.getStudents().size());
     }
 
     @Test
     public void testIfAvgIsCorrect(){
-        assertEquals(95.0, cohortWithMany.getCohortAverage(), 0);
+        assertEquals(89, cohortWithOne.getCohortAverage(), 0);
+        assertEquals();
     }
 
 }
